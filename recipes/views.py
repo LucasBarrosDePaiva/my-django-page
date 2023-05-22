04f96ff2d4515260cf782974d39593645e5e9981
lucasbarros <lucasbarros12@hotmail.com>
2023-05-22 10:02:09 -0300

    Complete header and search partials

--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,14 @@
-#from django.shortcuts import render
+from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def home(request):
-    return HttpResponse('Home')
+    return render(request, 'recipes/pages/home.html')
+
 
 def contact(request):
     return HttpResponse('Contact')
+
 
 def about(request):
     return HttpResponse('About')
